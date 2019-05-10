1d58897c72c70872de704816031dcea67985858b
Andrey Filipenkov <decapitator@ukr.net>
2019-05-10 18:37:15 +0300

    [tvos] add data to the top shelf

--- xbmc/utils/RecentlyAddedJob.cpp
+++ xbmc/utils/RecentlyAddedJob.cpp
@@ -25,6 +25,10 @@
 #include "video/VideoThumbLoader.h"
 #include "xbmc/settings/Settings.h"
 
+#if defined(TARGET_DARWIN_TVOS)
+#include "platform/darwin/tvos/TVOSTopShelf.h"
+#endif
+
 #define NUM_ITEMS 10
 
 CRecentlyAddedJob::CRecentlyAddedJob(int flag)
@@ -138,6 +142,11 @@ bool CRecentlyAddedJob::UpdateVideo()
     home->SetProperty("LatestEpisode." + value + ".SeasonThumb"   , "");
     home->SetProperty("LatestEpisode." + value + ".Fanart"        , "");
   }
+
+#if defined(TARGET_DARWIN_TVOS)
+    // send recently added Movies and TvShows to TopShelf
+    CTVOSTopShelf::GetInstance().SetTopShelfItems(items, TVShowItems);
+#endif
 
   i = 0;
   CFileItemList MusicVideoItems;
